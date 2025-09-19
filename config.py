e13fae8edf6ecda77a2c45376b1f5941ae4d66ed
Funkfanatiq <funkfanatiq@gmail.com>
2025-09-19 22:43:20 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -21,10 +21,3 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
     
-    # Настройки почты
-    MAIL_SERVER = 'smtp.mail.ru'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'pictortest@mail.ru'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'wFMvD0F25x93kvfw8bWP'
-    MAIL_DEFAULT_SENDER = 'pictortest@mail.ru'
